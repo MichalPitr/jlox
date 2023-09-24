2ba13fe77a340dd5f0f1ff622c319c4bbe0d2ca4
michal pitr <michal.pitr.altis@gmail.com>
2023-09-24 23:40:17 +0200

    started adding unary

--- c/compiler.c
+++ c/compiler.c
@@ -13,6 +13,11 @@ typedef struct {
 } Parser;
 
 Parser parser;
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() {
+    return compilingChunk;
+}
 
 static void errorAt(Token* token, const char* message) {
     // ignore further errors if already in panic mode.
@@ -62,10 +67,64 @@ static void consume(TokenType type, const char* message) {
     errorAtCurrent(message);
 }
 
+static void emitByte(uint8_t byte) {
+    writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+    emitByte(byte1);
+    emitByte(byte2);
+}
+
+static void emitReturn() {
+    emitByte(OP_RETURN);
+}
+
+static void emitConstant(Value value) {
+    addConstant(compilingChunk, value);
+}
+
+static void endCompiler() {
+    emitReturn();
+}
+
+static void grouping() {
+    expression();
+    consume(TOKEN_RIGHT_PAREN, "Expected ')' after an expression.");
+}
+
+static void number() {
+    // Simple starts walking from the starting pointer until it encounters a
+    // character no belonging to a number.
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+static void unary() {
+    // operator was already consumer, so retrieve its type from previous.
+    TokenType operatorType = parser.previous.type;
+
+    // Compile the operand.
+    expression();
+
+    // Emit theo operator instruction.
+    switch (operatorType) {
+        case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+        default: return; // This should be unreachable.
+    }
+}
+
+static void expression() {
+
+}
+
 bool compile(const char* source, Chunk* chunk) {
     initScanner(source);
+    // Makes the chunk available throughout the module.
+    compilingChunk = chunk;
     advance();
     expression();
     consume(TOKEN_EOF, "Expected end of expression.");
+    endCompiler();
     return !parser.hadError;
 }
