41905ded94d12c7d9ca56985f4352c0a04b6cbb6
michal pitr <michal.pitr.altis@gmail.com>
2023-09-26 12:21:26 +0200

    finish chapter 17

--- c/compiler.c
+++ c/compiler.c
@@ -5,12 +5,39 @@
 #include "compiler.h"
 #include "scanner.h"
 
+#ifdef DEBUG_PRINT_CODE
+#include "debug.h"
+#endif
+
+
 typedef struct {
     Token current;
     Token previous;
     bool hadError;
     bool panicMode;
 } Parser;
+
+typedef enum {
+    PREC_NONE, 
+    PREC_ASSIGNMENT, // =
+    PREC_OR, // or
+    PREC_AND, // and
+    PREC_EQUALITY, // == !=
+    PREC_COMPARISON, // < > <= >=
+    PREC_TERM, // + -
+    PREC_FACTOR, // * /
+    PREC_UNARY, // ! -
+    PREC_CALL, // . ()
+    PREC_PRIMARY
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
 
 Parser parser;
 Chunk* compilingChunk;
@@ -80,14 +107,48 @@ static void emitReturn() {
     emitByte(OP_RETURN);
 }
 
+// Not used. Using writeConstant instead to support more than 255 constants.
+static uint8_t makeConstant(Value value) {
+  int constant = addConstant(currentChunk(), value);
+  if (constant > UINT8_MAX) {
+    error("Too many constants in one chunk.");
+    return 0;
+  }
+
+  return (uint8_t)constant;
+}
+
 static void emitConstant(Value value) {
-    addConstant(compilingChunk, value);
+    // TODO: This might not work well. 
+    writeConstant(currentChunk(), value, parser.previous.line);
 }
 
 static void endCompiler() {
     emitReturn();
+#ifdef DEBUG_PRINT_CODE
+    if (!parser.hadError) {
+        disassambleChunk(currentChunk(), "code");
+    }
+#endif
 }
 
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
+
+static void binary() {
+  TokenType operatorType = parser.previous.type;
+  ParseRule* rule = getRule(operatorType);
+  parsePrecedence((Precedence)(rule->precedence + 1));
+
+  switch (operatorType) {
+    case TOKEN_PLUS:          emitByte(OP_ADD); break;
+    case TOKEN_MINUS:         emitByte(OP_SUBTRACT); break;
+    case TOKEN_STAR:          emitByte(OP_MULTIPLY); break;
+    case TOKEN_SLASH:         emitByte(OP_DIVIDE); break;
+    default: return; // Unreachable.
+  }
+}
 static void grouping() {
     expression();
     consume(TOKEN_RIGHT_PAREN, "Expected ')' after an expression.");
@@ -97,6 +158,7 @@ static void number() {
     // Simple starts walking from the starting pointer until it encounters a
     // character no belonging to a number.
     double value = strtod(parser.previous.start, NULL);
+
     emitConstant(value);
 }
 
@@ -104,8 +166,8 @@ static void unary() {
     // operator was already consumer, so retrieve its type from previous.
     TokenType operatorType = parser.previous.type;
 
-    // Compile the operand.
-    expression();
+    // Compile the operand. Nested Unary is allowed.
+    parsePrecedence(PREC_UNARY);
 
     // Emit theo operator instruction.
     switch (operatorType) {
@@ -114,8 +176,86 @@ static void unary() {
     }
 }
 
-static void expression() {
+// Array initializer syntax
+// Each [TOKEN_LEFT_PAREN] is an enum element, so translates to 
+// its corresponding enum index.
 
+// {grouping, NULL,   PREC_NONE} is a ParseRule and creates a struct.
+ParseRule rules[] = {
+  [TOKEN_LEFT_PAREN]    = {grouping, NULL,   PREC_NONE},
+  [TOKEN_RIGHT_PAREN]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LEFT_BRACE]    = {NULL,     NULL,   PREC_NONE}, 
+  [TOKEN_RIGHT_BRACE]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_COMMA]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_DOT]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_MINUS]         = {unary,    binary, PREC_TERM},
+  [TOKEN_PLUS]          = {NULL,     binary, PREC_TERM},
+  [TOKEN_SEMICOLON]     = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SLASH]         = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_STAR]          = {NULL,     binary, PREC_FACTOR},
+  [TOKEN_BANG]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_BANG_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EQUAL_EQUAL]   = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER]       = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_GREATER_EQUAL] = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_LESS_EQUAL]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IDENTIFIER]    = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_STRING]        = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NUMBER]        = {number,   NULL,   PREC_NONE},
+  [TOKEN_AND]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_CLASS]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ELSE]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FALSE]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FOR]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_FUN]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_IF]            = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_NIL]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_OR]            = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_PRINT]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_RETURN]        = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_SUPER]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_THIS]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_TRUE]          = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_VAR]           = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_WHILE]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_ERROR]         = {NULL,     NULL,   PREC_NONE},
+  [TOKEN_EOF]           = {NULL,     NULL,   PREC_NONE},
+};
+
+static void parsePrecedence(Precedence precedence) {
+    // parses tokens at the given precedence level or higher.
+    advance();
+
+    // First token of any expression always belongs to a prefix expression.
+    // If not, it's a syntax error. e.g. 5+3, 5 is a prefix expression, +3 is infix.
+    ParseFn prefixRule = getRule(parser.previous.type)->prefix;
+    
+    // If there is no prefix parser, it has to be a syntax error.
+    if (prefixRule == NULL) {
+        error("Expected an expression.");
+        return;
+    }
+
+    prefixRule();
+
+    // We only parse the infix if its precedence is high enough.
+    while (precedence <= getRule(parser.current.type)->precedence) {
+        advance();
+        ParseFn infixRule = getRule(parser.previous.type)->infix;
+        infixRule();
+    }
+}
+
+static ParseRule* getRule(TokenType type) {
+    // Returns a pointer to a ParseRule struct.
+    return &rules[type];
+}
+
+static void expression() {
+    // Lowest precedence, so will parse everything.
+    parsePrecedence(PREC_ASSIGNMENT);
 }
 
 bool compile(const char* source, Chunk* chunk) {
